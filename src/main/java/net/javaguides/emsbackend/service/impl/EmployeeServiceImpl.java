95ae4fc61d411f0b97657b6d2d22e3e6a52bf688
Vivek <vivek2511singh95@gmail.com>
2024-05-18 17:03:55 +0530

    Commit

--- src/main/java/net/javaguides/emsbackend/service/impl/EmployeeServiceImpl.java
+++ src/main/java/net/javaguides/emsbackend/service/impl/EmployeeServiceImpl.java
@@ -9,6 +9,9 @@ import net.javaguides.emsbackend.repository.EmployeeRepository;
 import net.javaguides.emsbackend.service.EmployeeService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
@@ -28,5 +31,12 @@ public class EmployeeServiceImpl implements EmployeeService {
         .orElseThrow(()->
                 new ResourceNotFoundException("Employee is not exist with given id:" + employeeId));
         return EmployeeMapper.mapToEmployeeDto(employee);
+    }
+
+    @Override
+    public List<EmployeeDto> getAllEmployee() {
+        List<Employee> employees = employeeRepository.findAll();
+        return employees.stream().map((employee) -> EmployeeMapper.mapToEmployeeDto(employee))
+                .collect(Collectors.toList());
     }
 }
